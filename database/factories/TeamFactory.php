48263c47c098e5a65a4e8fb6fb578db975e2deaa
Igor Finagin <Igor@Finag.in>
2019-03-26 12:51:02 +0000

    Apply fixes from StyleCI

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -3,7 +3,7 @@
 use App\Models\Team;
 use Faker\Generator as Faker;
 
-/**
+/*
  * @var \Illuminate\Database\Eloquent\Factory $factory
  */
 $factory->define(Team::class, static function (Faker $faker): array {
